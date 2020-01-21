2e17f33df8bc7bd4f82164e95832d80e84063668
Troy Neubauer <troyneubauer@gmail.com>
2020-01-20 22:24:06 -0500

    fix 12

--- .github/CI/build.py
+++ .github/CI/build.py
@@ -16,8 +16,42 @@ print('buildConfiguration ' + buildConfiguration)
 if (compiler == 'emcc'):
 	os.system('source emsdk-master/emsdk_env.sh')
 os.system('cp -r .github/CI/CI\\ Project/* ..')
-os.system('ls')
-os.system('pwd')
+#move to the new root of the project
+os.chdir('..')
+
+
+premakeCommand = ''
+if osName == 'linux':
+	premakeCommand += 'premake5 '
+
+elif osName == 'windows':
+	premakeCommand += 'Hazel/vendor/premake/bin/premake5.exe '
+
+elif osName == 'macosx':
+	print('osx is unsupported for now!')
+	sys.exit(1)
+
+else:
+	print('Unknown OS! ' + osName)
+	sys.exit(1)
+
+
+
+if compiler == 'msvc':
+	premakeCommand += '--os=windows --compiler=msc vs2017'
+
+elif compiler == 'gcc':
+	premakeCommand += '--os=' + osName + ' --compiler=gcc gmake2'
+
+elif compiler == 'clang':
+	premakeCommand += '--os=' + osName + ' --compiler=gcc gmake2'
+
+else
+	print('Unknown compiler! ' + compiler)
+	sys.exit(1)
+
+print('running premake: \"' + premakeCommand + '\"')
+os.system(premakeCommand)
 
 #We dont need these vars. The makefile runs the right compiler anyway
 #unset CC
@@ -33,7 +67,7 @@ os.system('pwd')
 #msbuild /p:Configuration=$BUILD_CONFIG
 #fi
 
-#        - PREMAKE_CMD='"premake5 --os=emscripten --scripts=Hazel/vendor/premake/scripts gmake2"'
+#        - PREMAKE_CMD='" --os=emscripten --scripts=Hazel/vendor/premake/scripts gmake2"'
 #        - PREMAKE_CMD='"Hazel\vendor\premake\bin\premake5.exe --os=windows --compiler=msc vs2017"'
 #        - PREMAKE_CMD='"premake5 --os=linux --compiler=gcc gmake2"'
 #        - PREMAKE_CMD='"premake5 --os=linux --compiler=clang gmake2"'
