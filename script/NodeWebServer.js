a0fe7a611714f2756709519be59baa8b3b2c9596
Troy Neubauer <troyneubauer@gmail.com>
2020-01-30 15:48:50 -0500

    Fixed emscripten web server

--- script/NodeWebServer.js
+++ script/NodeWebServer.js
@@ -1,11 +1,12 @@
 
-if (process.argv.length == 2)
+
+if (process.argv.length < 3)
 {
 	console.log("ERROR: You must specify the directory to host files from!");
 }
 else
 {
-	console.log(process.argv[2]);
+	console.log("Using dir: " + process.argv[2]);
 	const express = require('express');
 	const app = express();
 	app.use(express.static(process.argv[2]));
